800427c787ec11518c6e4627109fbee0fa016b0b
sixsixfive <punx665@gmail.com>
2017-04-25 18:18:06 +0200

    Tue Apr 25 18:18:06 CEST 2017

---
+++ .changecolor.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+if [ ! -t 0 ]; then
+	x-terminal-emulator -e "$0"
+	exit 0
+fi
+basedir="$(dirname "$(readlink -f "${0}")")"
+cd "$basedir"
+if [ ! -f "$basedir"/.changecolor.sh ]; then
+	exit 1
+fi
+color="$1"
+case $color in
+	-c|--color)
+	newcolor="$2";;
+esac
+###things we need here
+type sed >/dev/null 2>&1 &&
+type tr >/dev/null 2>&1 &&
+type find >/dev/null 2>&1||_missingdep=true
+case $_missingdep in
+	true)
+		printf "You either miss sed, tr, or find! Aborting!\n"
+		exit 1;;
+esac
+###set the color
+if [ -z ${newcolor+x} ]; then 
+	if type yad >/dev/null 2>&1; then
+		newcolor=$(yad --color --init-color=#5288d1 --title="Please select a new #RRGGBB hilight color")
+	elif type qarma >/dev/null 2>&1; then
+		newcolor=$(qarma --color-selection --color=#5288d1 --title="Please select a new #RRGGBB hilight color")
+	else
+		read -p "Please enter your new hilight color in #RRGGBB(The '#' is a must!, #5288d1 is currently set): " newcolor
+	fi
+fi
+if [ -z ${newcolor+x} ]; then
+	printf "no color was was selected, aborting!"
+	exit 1
+fi
+case "${newcolor//#/}|tr '[:upper:]' '[:lower:]" in
+	424141|f1f2f2|656666|323131|5288d1|2980b9|bdc3c7|cc0000|3366cc|ff6600|669900)
+		printf "this color is not available, aborting!"
+		exit 1;;
+esac
+###recolor the xfwm/ui themes
+_folders="images gtk-3.0 gtk-2.0 xfwm4 xfdashboard-1.0"
+for _folder in $_folders; do
+	cd $_folder
+	find "$basedir/$_folder" -type f -exec sed -i 's/#5288d1/'$newcolor'/g' {} \;
+	cd $basedir
+done
+sed -i 's/#5288d1/'$newcolor'/g' .changecolor.sh
+printf "\n\ndone\n\n"
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ## Phlat
-simple, dark theme made for Xfce
+simple and fast, dark theme made for Xfce.
 
-Depends on: gtk2-mist, murrine and pixbuf engine
+Depends on: GTK(mist, murrine and pixbuf engine) and GTK3>=3.20, 
+
+Note: The GTK3 theme recommends [GTK3-NOCSD](https://github.com/PCMan/gtk3-nocsd) and to disable the GTK3 overlay scrollbars (export GTK_OVERLAY_SCROLLING=0)!
+
+### Optional
+- If you want to change the blue highlight color to something else:
+
+ * needs sed, tr and find! - you may should install [yad](http://sourceforge.net/projects/yad-dialog) or [qarma-qt5](https://github.com/luebking/qarma) for a better color selection.
+
+```
+sh .changecolor.sh
+```
